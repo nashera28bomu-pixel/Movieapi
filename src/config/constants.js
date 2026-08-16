b21df0ac3f8f458674287b932cdd80f5d2751009
nashera28bomu-pixel <nashera28bomu@gmail.com>
2026-08-16 17:18:16 +0300

    Add constants for API configuration

---
+++ src/config/constants.js
@@ -0,0 +1,9 @@
+export const UPSTREAM_BASE_URL = process.env.UPSTREAM_BASE_URL || "https://movieapi.xcasper.space";
+
+export const DEFAULT_TIMEOUT = 15000; // 15 seconds
+
+export const DEFAULT_HEADERS = {
+  "Accept": "application/json",
+  "Content-Type": "application/json",
+  "User-Agent": "CymorMovieHub-Backend/1.0",
+};
